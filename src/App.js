5491713ade5aaf2020b375a9843a718c5cebf65a
unknown <>
2022-01-19 16:11:33 +0900

    project init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import MainPageComponent from "./main/index.js"
+
+
+function App() {
+
+  return (<div>
+    <MainPageComponent />;
+
+  </div>
+  );
+}
+
+export default App;
