bf643e03a3da9a01331a7dadc8da68d414715f7b
unknown <>
2022-02-07 19:25:28 +0900

    업데이트 완료

--- src/main/index.js
+++ src/main/index.js
@@ -1,57 +1,90 @@
-
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 import axios from "axios";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { API_URL } from "../config/constants.js";
+import { Carousel } from "antd";
+
+dayjs.extend(relativeTime);
 
 function MainPage() {
-    const [products, setProducts] = React.useState([]);
+  const [products, setProducts] = React.useState([]);
+  const [banners, setBanners] = React.useState([]);
   React.useEffect(function () {
     axios
-      .get("https://1e7c4a0c-1ec7-46ce-85d2-387ca44d8695.mock.pstmn.io/products")
+      .get(`${API_URL}/products`)
       .then(function (result) {
         const products = result.data.products;
         setProducts(products);
       })
       .catch(function (error) {
-        console.error('에러 발생 : ', error);
+        console.error("에러 발생 : ", error);
+      });
+
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error("에러 발생 : ", error);
       });
   }, []);
-    
-    return ( 
+
+  return (
     <div>
-      <div id="banner">
-        <img src="images/banners/banner1.png" />
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageUrl}`} />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
+      <h1 id="product-headline">판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              {product.soldout === 1 && <div className="product-blur" />}
+              <Link className="product-link" to={`/products/${product.id}`}>
+                <div>
+                  <img
+                    className="product-img"
+                    src={`${API_URL}/${product.imageUrl}`}
+                  />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-footer">
+                    <div className="product-seller">
+                      <div>
+                        <img
+                          className="product-avatar"
+                          src="images/icons/avatar.png"
+                        />
+                        <span>{product.seller}</span>
+                      </div>
+                      <span className="product-date">
+                        {dayjs(product.createdAt).fromNow()}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <h1>판매되는 상품들</h1>
-                <div id="product-list">
-            {products.map(function (product, index) {
-              return (
-                      <div className="product-card">
-                  <Link className="product-link" to={`/product/${product.id}`}>
-                        <div>
-                                <img className="product-img" src={product.imageUrl}/>
-                        </div>
-                        <div className="product-contents">
-                                <span className="product-name">{product.name}
-                        
-                            </span>
-                            <span className="product-price">{product.price}원
-                                
-                            </span>
-                            <div className="product-seller">
-                                <img className="product-avatar" src="images/icons/avatar.png" />
-                                    <span>{product.seller}</span>
-                            </div>
-                          </div>
-                          </Link>
-                    </div>)})}
-                  
-      </div>
-      
     </div>
-   
-    )
+  );
 }
 
-export default MainPage
+export default MainPage;
